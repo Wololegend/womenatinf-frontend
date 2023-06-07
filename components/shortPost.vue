e480a4563ff71c9c6d43001cfa215f65cbf7e0cb
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2023-06-07 11:38:02 +0100

    Finished the structure of the main page. TODO: Add nuxt-links for dynamic article pages

--- components/shortPost.vue
+++ components/shortPost.vue
@@ -1,37 +1,66 @@
-<template>
+<template >
     <div class="block columns is-vcentered">
-        <div class="column is-3">
-            <b-image
+        <div :class="imgClass">
+            <!-- <b-image
+                style="height:10rem"
                 :src="img"
                 :alt="'Imagen del post' + title"
-                ratio="1by1"
+                :ratio="ratioImg"
                 responsive
-            ></b-image>
+            ></b-image> -->
+
+            <img
+              :src="img"
+              :alt="'Imagen del post' + title"
+            >
         </div>
 
-        <div class="block column is-11 mx-5">
-            <h3 class="title mt-3"> {{ title }} </h3>
+        <div :class="txtClass" :style="widthText">
+            <h3 :class="titleClass" :style="colorTitle"> {{ title }} </h3>
 
-            <p class="content"> {{ textPreview }} </p>
+            <!-- <p :class="contentClass"> {{ textPreview }} </p> -->
+
+            <template lang="md">
+              <div v-html="$md.render(textPreview)"></div>
+            </template>
         </div>
     </div>
   </template>
   
   <script>  
   export default {
-    name: 'textPreviewPost',
-    props: ['img', 'title', 'text'],
+    name: 'shortPost',
+    props: ['img', 'title', 'text', 'small'],
     data() {
       return {
         textPreview: '',
-        maxCharsPerLine: 130
+        maxCharsPerLine: 130,
+        imgClass: 'column is-3',
+        txtClass: 'block column is-11 mx-5',
+        ratioImg: '1by1',
+        titleClass: 'title mt-3',
+        contentClass: 'content',
+        colorTitle: 'color: #121212',
+        widthText: 'width: 70%'
       }
     },
-    async mounted() {
-      if (this.text.length > this.maxCharsPerLine)
+    async created() {
+      if (this.small) {
+        this.imgClass = 'column is-half ml-4 mr-2'
+        this.txtClass = 'block column is-half'
+        this.ratioImg = ''
+        this.titleClass = 'mt-3 is-size-5',
+        this.contentClass = 'content is-size-7'
+        this.colorTitle = 'color: #fff'
+        this.widthText = 'width: 40%'
+        this.maxCharsPerLine = 50
+      }
+
+      if (this.text.length > this.maxCharsPerLine) {
         this.textPreview = this.text.slice(0, this.maxCharsPerLine)
 
         this.textPreview += '...'
+      }
     },
   }
   </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,162 @@
 <template>
-
   <!-- There are four <sections>. Each one of them is a tile that contains: the title, the "popular" section, the "local", and the rest of the posts. -->
 
-  <div class="tile is-ancestor columns is-multiline my-5 mx-5">
+  <div class="tile is-ancestor is-flex-wrap-wrap mt-4">
+    <div class="tile is-parent is-12" style="height: 100%">
+      <article v-if="boolean && popular.length > 0"
+        class="tile is-child notification is-flex is-flex-direction-column is-justify-content-center is-align-items-center titleTile titleTileBorder">
+        <h1 class="title" style=""> Nuestros logros más recientes </h1>
+      </article>
+    </div>
 
-    <!-- TITLE -->
-    <section class="tile is-parent is-12">
-      <article class="tile is-child notification titleTile is-full has-text-centered">
+    <div class="tile is-vertical is-8">
+      <div class="tile is-parent">
+        <article v-if="boolean && popular.length > 0" class="tile is-child notification customMainTile">
+          <div class="is-vcentered my-2 container">
+            <h2 class="title ml-3"> Proyectos destacados </h2>
+
+            <hr class="betweenTitleMain">
+
+
+              <short-post class="ml-3 hoverMain" :img="'http://localhost:1337' + popular[0].attributes.media.data[0].attributes.url"
+              :title="popular[0].attributes.titulo" :text="popular[0].attributes.cuerpo"></short-post>
+
+
+            <hr class="betweenPostsMain is-centered" v-if="popular.length > 1">
+
+            <short-post class="ml-3 mb-1 hoverMain" v-if="popular.length > 1"
+              :img="'http://localhost:1337' + popular[1].attributes.media.data[0].attributes.url"
+              :title="popular[1].attributes.titulo" :text="popular[1].attributes.cuerpo"></short-post>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent">
+        <article v-if="boolean && local.length > 0" class="tile is-child notification customMainTile">
+          <div class="is-vcentered my-2 container">
+            <h2 class="title ml-3"> Proyectos locales </h2>
+
+            <hr class="betweenTitleMain">
+
+            <short-post class="ml-3 hoverMain" :img="'http://localhost:1337' + local[0].attributes.media.data[0].attributes.url"
+              :title="local[0].attributes.titulo" :text="local[0].attributes.cuerpo"></short-post>
+
+            <hr class="betweenPostsMain is-centered" v-if="local.length > 1">
+
+            <short-post class="ml-3 mb-1 hoverMain" v-if="local.length > 1"
+              :img="'http://localhost:1337' + local[1].attributes.media.data[0].attributes.url"
+              :title="local[1].attributes.titulo" :text="local[1].attributes.cuerpo"></short-post>
+          </div>
+        </article>
+      </div>
+    </div>
+    <div class="tile is-parent">
+      <article v-if="boolean && rest.length > 0" class="tile is-child notification customSideTile">
+        <div class="content">
+          <div class="is-vcentered container mt-3">
+            <h2 class="title"> Más iniciativas </h2>
+
+            <hr class="betweenTitleSide">
+
+            <template v-for="(post, index) in this.rest">
+              <short-post class="hoverSide ml-1" :img="'http://localhost:1337' + post.attributes.media.data[0].attributes.url"
+              :title="post.attributes.titulo" :text="post.attributes.cuerpo" :small="true"></short-post>
+
+              <hr class="betweenPostsSide is-centered" v-if="index < 4">
+            </template>
+
+            <!-- <short-post :img="'http://localhost:1337' + rest[0].attributes.media.data[0].attributes.url"
+              :title="rest[0].attributes.titulo" :text="rest[0].attributes.cuerpo" :small="true"></short-post>
+
+            <hr class="betweenPostsSide is-centered" v-if="rest.length > 1">
+
+            <short-post v-if="rest.length > 1"
+              :img="'http://localhost:1337' + rest[1].attributes.media.data[0].attributes.url"
+              :title="rest[1].attributes.titulo" :text="rest[1].attributes.cuerpo" :small="true"></short-post>
+
+            <hr class="betweenPostsSide is-centered" v-if="rest.length > 1">
+
+            <short-post v-if="rest.length > 2"
+              :img="'http://localhost:1337' + rest[1].attributes.media.data[0].attributes.url"
+              :title="rest[1].attributes.titulo" :text="rest[1].attributes.cuerpo" :small="true"></short-post>
+
+            <hr class="betweenPostsSide is-centered" v-if="rest.length > 1">
+
+            <short-post v-if="rest.length > 3"
+              :img="'http://localhost:1337' + rest[1].attributes.media.data[0].attributes.url"
+              :title="rest[1].attributes.titulo" :text="rest[1].attributes.cuerpo" :small="true"></short-post> -->
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+
+  <!-- <div class="columns is-multiline is-centered my-5">
+    <section class="column is-full titleTileBorder">
+      <article class="notification titleTile is-full has-text-centered">
         <h1 class="title is-inline-block" style=""> Nuestros logros más recientes </h1>
       </article>
     </section>
 
-    <!-- POPULAR -->
-    <section class="tile is-parent column is-8 customTile mx-3">
-      <div class="columns is-multiline is-vcentered my-2 container">
+    <section v-if="boolean && popular.length > 0" class="column is-8 customTile mt-3">
+      <div class="is-vcentered my-2 container">
         <h2 class="title ml-3"> Destacados </h2>
 
-        <hr class="betweenTitle">
+        <hr class="betweenTitleMain">
 
         <short-post
           class="ml-3"
-          v-if="boolean"
-          :img="'http://localhost:1337' + posts[0].attributes.media.data[0].attributes.url"
-          :title="posts[0].attributes.titulo"
-          :text="posts[0].attributes.cuerpo"
+          :img="'http://localhost:1337' + popular[0].attributes.media.data[0].attributes.url"
+          :title="popular[0].attributes.titulo"
+          :text="popular[0].attributes.cuerpo"
         ></short-post>
 
-        <hr class="betweenPosts is-centered">
+        <hr 
+          class="betweenPostsMain is-centered"
+          v-if="popular.length > 1"  
+        >
 
         <short-post
-          class="ml-3"
-          v-if="boolean"
-          :img="'http://localhost:1337' + posts[1].attributes.media.data[0].attributes.url"
-          :title="posts[1].attributes.titulo"
-          :text="posts[1].attributes.cuerpo"
+          class="ml-3 mb-1"
+          v-if="popular.length > 1"
+          :img="'http://localhost:1337' + popular[1].attributes.media.data[0].attributes.url"
+          :title="popular[1].attributes.titulo"
+          :text="popular[1].attributes.cuerpo"
         ></short-post>
       </div>
     </section>
-  </div>
-</template>
+
+    <section v-if="boolean && popular.length > 0" class="column is-1  mt-3">
+      
+    </section>
+
+    <section v-if="boolean && popular.length > 0" class="column is-3 customTile mt-3">
+      <div class="is-vcentered my-2 container">
+        <h2 class="title ml-3"> Destacados </h2>
+
+        <hr class="betweenTitleMain">
+
+        <short-post
+          class="ml-3"
+          :img="'http://localhost:1337' + popular[0].attributes.media.data[0].attributes.url"
+          :title="popular[0].attributes.titulo"
+          :text="popular[0].attributes.cuerpo"
+        ></short-post>
+
+        <hr 
+          class="betweenPostsMain is-centered"
+          v-if="popular.length > 1"  
+        >
+
+        <short-post
+          class="ml-3 mb-1"
+          v-if="popular.length > 1"
+          :img="'http://localhost:1337' + popular[1].attributes.media.data[0].attributes.url"
+          :title="popular[1].attributes.titulo"
+          :text="popular[1].attributes.cuerpo"
+        ></short-post>
+      </div>
+    </section> -->
+</div></template>
 
 <script>
 import axios from 'axios'
@@ -50,6 +168,9 @@ export default {
   data() {
     return {
       posts: [],
+      popular: [],
+      local: [],
+      rest: [],
       boolean: false
     }
   },
@@ -61,16 +182,41 @@ export default {
       await axios.get('http://localhost:1337/api/publicaciones?populate=media')
         .then(response => {
           new Promise((resolve, reject) => {
-            console.log(response.data)
-
             this.posts = response.data.data
 
             this.posts.sort((item1, item2) => {
               return item2.attributes.publishedAt - item1.attributes.publishedAt
             })
 
+            // this.popular = this.posts.filter(post => { post.attributes.Tipos == 'Destacados' })
+
+            this.posts.forEach(post => {
+              if (post.attributes.Tipos == 'Destacados')
+                this.popular.push(post)
+              else if (post.attributes.Tipos == 'Locales')
+                this.local.push(post)
+              else
+                this.rest.push(post)
+            })
+
             resolve()
           }).then(() => {
+            // this.posts.forEach(post => {
+            //   switch (post.attributes.Tipos) {
+            //     case "Destacados":
+            //       this.popular.push(post)
+            //     break;
+
+            //     case "Locales":
+            //       this.local.push(post)
+            //     break;
+
+            //     case "No destacados":
+            //       this.rest.push(post)
+            //     break;
+            //   }
+            // })         
+
             this.boolean = true
 
             loadingComponent.close()
@@ -87,31 +233,71 @@ export default {
 </script>
 
 <style scoped>
-.customTile {
+.customMainTile {
   background-color: #f0eff4;
-  color: #121212
+  color: #121212;
+  border-radius: 5px;
+}
+
+.customSideTile {
+  background-color: #32576e;
+  color: #fff;
+  border-radius: 5px;
 }
 
 .titleTile {
-  color: #5a869b;
+  color: #32576e;
   background-color: #fff;
-  border: 3px dashed #5a869b;
+  height: 15%
 }
 
-hr.betweenPosts {
-    width: 96%;
-    border-color: #32576e;
-    background-color: #32576e;
-    height: 1px;
-    margin: 1rem auto 2rem auto
+.titleTileBorder {
+  border: 3px dashed #32576e;
+  border-radius: 5px;
+  height: 7rem
 }
 
-hr.betweenTitle {
-    width: 96%;
-    border-color: #32576e;
-    background-color: #32576e;
-    height: 1px;
-    margin: 0 auto 2rem auto
+.betweenPostsMain {
+  width: 96%;
+  border-color: #32576e;
+  background-color: #32576e;
+  height: 1px;
+  margin: 1rem auto 2rem auto
 }
 
+.betweenTitleMain {
+  width: 96%;
+  border-color: #32576e;
+  background-color: #32576e;
+  height: 2px;
+  margin: 0 auto 2rem auto
+}
+
+.betweenPostsSide {
+  width: 96%;
+  border-color: #f0eff4;
+  background-color: #f0eff4;
+  height: 1px;
+  margin: 1rem auto 2rem auto
+}
+
+.betweenTitleSide {
+  width: 96%;
+  border-color: #f0eff4;
+  background-color: #f0eff4;
+  height: 2px;
+  margin: 0 auto 2rem auto
+}
+
+.hoverMain:hover {
+  background-color: #d1d1d3;
+}
+
+.hoverSide:hover {
+  background-color: #336381;
+}
+
+.nuxtLinkDecoration {
+  text-decoration: none !important;
+}
 </style>
